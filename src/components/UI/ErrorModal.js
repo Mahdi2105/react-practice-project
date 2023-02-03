0738240eead8ebb98de2dc38160c0fcfbe7c3b1e
Mahdi Hasan <mhasan@westminster.gov.uk>
2023-02-03 11:40:46 +0000

    Added portals

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
 import Card from "./Card";
 import Button from "./Button";
 import "./ErrorModal.css";
 
+const Backdrop = (props) => {
+  return <div className="backdrop" onClick={props.onExitError} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className="modal">
+      <header className="header">
+        <h2>{props.title}</h2>
+      </header>
+      <div className="content">
+        <p>{props.message}</p>
+      </div>
+      <footer className="actions">
+        <Button onClick={props.onExitError}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = (props) => {
   return (
-    <div>
-      <div className="backdrop" onClick={props.onExitError} />
-      <Card className="modal">
-        <header className="header">
-          <h2>{props.title}</h2>
-        </header>
-        <div className="content">
-          <p>{props.message}</p>
-        </div>
-        <footer className="actions">
-          <Button onClick={props.onExitError}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onExitError={props.onExitError} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onExitError={props.onExitError}
+        />,
+        document.getElementById("backdrop-root")
+      )}
+    </Fragment>
   );
 };
 
